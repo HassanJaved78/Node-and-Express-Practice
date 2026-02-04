e9e00611cc83038f7f7338d356f57214c2290945
Hassan Javed <hassanjaved0554@gmail.com>
2026-02-04 17:45:11 +0500

    entered data into table

---
+++ Express full API/seedTable.js
@@ -0,0 +1,37 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import path from 'node:path'
+import { vinyl } from "./data.js";
+
+async function seedTable() {
+    const db = await open({
+        filename: path.join('database.db'),
+        driver: sqlite3.Database
+    })
+
+    try{
+        await db.exec('BEGIN TRANSACTION');
+
+        for (const { title, artist, price, year, genre, stock } of vinyl) {
+            await db.run(
+                `INSERT INTO products (title, artist, price, year, genre, stock)
+                VALUES(?, ?, ?, ?, ?, ?)`,
+                [title, artist, price, year, genre, stock]
+            )
+        }
+        
+        await db.exec('COMMIT');
+        console.log("All records inserted successfully");
+    }
+    catch(err){
+        await db.exec('ROLLBACK');
+        console.log("Error inserting data: ", err.message);
+    } 
+    finally{
+        await db.close();
+        console.log("Database connection closed successfully");
+    }
+
+}
+
+seedTable();
